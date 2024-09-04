d6a19fd8d930a86341468267dbea9bd16d739141
Bernard Moy <bernardmoy3@gmail.com>
2024-09-04 21:19:33 +0800

    Shopping list adapter

--- app/src/main/java/com/example/recipeapp/ShoppingListAdapter.java
+++ app/src/main/java/com/example/recipeapp/ShoppingListAdapter.java
@@ -1,24 +1,44 @@
 package com.example.recipeapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingListAdapter extends RecyclerView.Adapter<ShoppingListRecyclerViewHolder> {
+
+    private Context ctx;
+    private ArrayList<ShoppingListPreview> shoppingListPreviewList;
+
+    public ShoppingListAdapter(Context ctx, ArrayList<ShoppingListPreview> shoppingListPreviewList){
+        this.ctx = ctx;
+        this.shoppingListPreviewList = shoppingListPreviewList;
+    }
+
     @NonNull
     @Override
     public ShoppingListRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ShoppingListRecyclerViewHolder(LayoutInflater.from(ctx).inflate(R.layout.shopping_list_row, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ShoppingListRecyclerViewHolder holder, int position) {
 
+        ShoppingListPreview shoppingListPreview = shoppingListPreviewList.get(position);
+
+        holder.getNameTextView().setText(shoppingListPreview.getName());
+        holder.getItemCountTextView().setText(String.valueOf(shoppingListPreview.getItemCount()));
+        holder.getSupermarketCountTextView().setText(String.valueOf(shoppingListPreview.getSupermarketCount()));
+        holder.getCostTextView().setText(String.valueOf(shoppingListPreview.getCost()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return shoppingListPreviewList.size();
     }
 }
