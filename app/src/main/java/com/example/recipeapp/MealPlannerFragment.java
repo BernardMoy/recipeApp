f842fbf7be593b89641052809a6ee97dc038012d
Bernard Moy <bernardmoy3@gmail.com>
2024-09-08 01:59:17 +0800

    Basic default calendar view

--- app/src/main/java/com/example/recipeapp/MealPlannerFragment.java
+++ app/src/main/java/com/example/recipeapp/MealPlannerFragment.java
@@ -1,12 +1,17 @@
 package com.example.recipeapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MealPlannerFragment extends Fragment {
+
+    // main calendarview
+    private CalendarView calendarView;
+    private Calendar calendar;
+    private Context ctx;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +71,19 @@ public class MealPlannerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal_planner, container, false);
+        View view = inflater.inflate(R.layout.fragment_meal_planner, container, false);
+        ctx = view.getContext();
+
+        // find calendarview
+        calendarView = view.findViewById(R.id.calendarView);
+        calendar = Calendar.getInstance();
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+            }
+        });
+
+        return view;
     }
 }
