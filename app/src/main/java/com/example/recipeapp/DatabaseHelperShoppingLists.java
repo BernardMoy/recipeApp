9d27d9376a1378f918651ece23d8a749fbbb6961
Bernard Moy <bernardmoy3@gmail.com>
2024-09-21 12:37:06 +0800

    Display total amount instead of distinct amount in SL

--- app/src/main/java/com/example/recipeapp/DatabaseHelperShoppingLists.java
+++ app/src/main/java/com/example/recipeapp/DatabaseHelperShoppingLists.java
@@ -99,7 +99,7 @@ public class DatabaseHelperShoppingLists extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
 
         // Extract all SL data
-        String queryShoppingList = "SELECT COUNT(I.ingredient_id), COUNT(DISTINCT supermarket), SUM(I.cost*I.amount) " +
+        String queryShoppingList = "SELECT SUM(I.amount), COUNT(DISTINCT supermarket), SUM(I.cost*I.amount) " +
                 "FROM Shopping_lists SL "+
                 "JOIN Shopping_list_supermarket_ingredients SLSI ON SL.shopping_list_id = SLSI.shopping_list_id "+
                 "JOIN Ingredients I ON SLSI.ingredient_id = I.ingredient_id " +
